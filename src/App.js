351c63bc46f8bf57b5d0c8ef805db3984192d8d9
Patchanida <nikarostskilled@gmail.com>
2020-04-18 13:04:48 +0700

    add route to page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Home } from './components/Home/Home';
+import Header from './components/Header';
+import Footer from './components/Footer'
+class App extends React.Component {
 
-class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Router>
+          <div className="App">
+            <Header>
+            </Header>
+            <Switch>
+              <Route sensitive strict exact path="/" component={Home} />
+            </Switch>
+            <Footer></Footer>
+          </div>
+        </Router>
       </div>
     );
   }
