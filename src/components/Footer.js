10135c33927733bb01a64230d82fa1bf3743eefa
Patchanida <nikarostskilled@gmail.com>
2020-04-18 13:02:46 +0700

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <span>Patchanida Kittpongpattana (Jane)</span><br />
+                <span>Contact: (+66)91 747 6604</span><br />
+                <span>Email: patchanida.ktp@gmail.com</span>
+            </div>
+        )
+    }
+}
+
+export default Footer
