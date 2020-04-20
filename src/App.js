024f1d3da5265e5a527ab8a3f9b4232fb004d969
Patchanida <nikarostskilled@gmail.com>
2020-04-20 09:56:09 +0700

    add route resume

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Home } from './components/Home/Home';
+import { Resume } from './components/Resume/Resume';
 import Header from './components/Header';
 import Footer from './components/Footer'
 class App extends React.Component {
@@ -13,9 +13,12 @@ class App extends React.Component {
           <div className="App">
             <Header>
             </Header>
-            <Switch>
-              <Route sensitive strict exact path="/" component={Home} />
-            </Switch>
+            <div className="content text-left">
+              <Switch>
+                <Route sensitive strict exact path="/" component={Resume} />
+                <Route sensitive strict exact path="/resume" component={Resume} />
+              </Switch>
+            </div>
             <Footer></Footer>
           </div>
         </Router>
